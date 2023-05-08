f240b2b5eed809f834bb429c9c7994b80ccd74ca
Djum542 <djum54022@gmail.com>
2023-05-08 08:58:23 +0700

    message test

--- messanger/app/src/main/java/com/java/messanger/MainActivity.java
+++ messanger/app/src/main/java/com/java/messanger/MainActivity.java
@@ -2,11 +2,15 @@ package com.java.messanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 // khởi tạo sự kiện
+//    TextView text = this.findViewById(R.id.mess);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     }
     //sử lý sự kiện khi người dùng nhấn  click
     public void onsendmessage(View view){
-
+        EditText mesageView = (EditText) findViewById(R.id.message);
+        String mesageText = mesageView.getText().toString();
+//      intent.putExtra("mesage", mesageText);
+        Intent intent = new Intent(this, ReceiMesage.class);
+        intent.putExtra(ReceiMesage.EXTRA_MESSAGE, mesageText);
+        startActivity(intent);
+        // lấy chuỗi qua intent .
+//        Intent intent1 = getIntent();
+//        String string = intent1.getStringExtra("mesage");
+//        int intNum = intent.getIntExtra("name", text);
     }
 }
--- messanger/app/src/main/java/com/java/messanger/ReceiMesage.java
+++ messanger/app/src/main/java/com/java/messanger/ReceiMesage.java
@@ -2,13 +2,22 @@ package com.java.messanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiMesage extends AppCompatActivity {
+// giá trị extra đưa cho intent
+    public static final String EXTRA_MESSAGE = "mesage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recei_mesage);
+        Intent intent = getIntent();
+        String mesageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView mesageView = (TextView) findViewById(R.id.message);
+        mesageView.setText(mesageText);
+        // thêm một text từ message textview
     }
 }
